81140974c463bb9e3f1c409453064b246cbc817e
amiholz <amiholz@cs.huji.ac.il>
2019-03-06 20:03:23 +0200

    update custom

--- policies/policy_custom.py
+++ policies/policy_custom.py
@@ -7,18 +7,17 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.datasets import mnist
 
 GAMMA = 0.9
-SHIFT = 1
-EPSILON = 0.5
-MIN_EPSILON = 0.01
+EPSILON = 0.6
+MIN_EPSILON = 0.1
 EPSILON_RATE = 0.9
 ROTATIONS = {'N':0, 'S':2, 'W':-1, 'E':1}
 NUM_OF_FEATURES = 11
 WINDOW_SIZE = 9
 EPSILON_ROUNDS = 20
-MIN_BATCH_SIZE = 16
+MIN_BATCH_SIZE = 8
 MAX_BATCH_SIZE = 256
-NN = 20
-
+NN = 30
+SHIFT = 1
 class Custom(bp.Policy):
     """
     A policy which avoids collisions with obstacles and other snakes. It has an epsilon parameter which controls the
@@ -32,7 +31,6 @@ class Custom(bp.Policy):
         policy_args['epsilon_rounds'] = float(policy_args['epsilon_rounds']) if 'epsilon_rounds' in policy_args else EPSILON_ROUNDS
         policy_args['batch_size'] = float(policy_args['batch_size']) if 'batch_size' in policy_args else MAX_BATCH_SIZE
         policy_args['gamma'] = float(policy_args['gamma']) if 'gamma' in policy_args else GAMMA
-        policy_args['shift'] = float(policy_args['shift']) if 'shift' in policy_args else SHIFT
         return policy_args
 
     def init_run(self):
@@ -40,6 +38,7 @@ class Custom(bp.Policy):
         print("NN:", NN)
         self.model = Sequential()
         self.model.add(Dense(NN, activation='relu', input_shape=(WINDOW_SIZE*NUM_OF_FEATURES,)))
+        self.model.add(Dense(NN, activation='relu'))
         self.model.add(Dense(1))
         self.model.compile(loss='mean_squared_error',
                       optimizer='adam',
@@ -51,9 +50,9 @@ class Custom(bp.Policy):
         self.rewards = np.zeros(self.batch_size)
         self.batches_to_take = MIN_BATCH_SIZE
         self.full_batch = False
-        self.last_mask = None
-        self.masks = np.zeros((self.batch_size,3, 3))
-        self.next_masks = np.zeros((self.batch_size,3, 3, 3))
+        # self.last_mask = None
+        # self.masks = np.zeros((self.batch_size,3, 3))
+        # self.next_masks = np.zeros((self.batch_size,3, 3, 3))
 
     def learn(self, round, prev_state, prev_action, reward, new_state, too_slow):
         try:
@@ -61,7 +60,7 @@ class Custom(bp.Policy):
             # case we did not collect enough samples
             if not self.full_batch:
                 self.batches_to_take = self.batch_counter
-
+            print("size of batch:", self.batches_to_take)
             sort_rewards = np.argsort(self.rewards) # sort rewards because we want the best learning
             indices_to_take = sort_rewards[-self.batches_to_take:]
 
@@ -103,55 +102,51 @@ class Custom(bp.Policy):
         board, head = new_state
         head_pos, direction = head
         rows, cols = board.shape
-        # head_value = board[head_pos[0], head_pos[1]]
         head_value = board[head_pos[0], head_pos[1]]
-        # print("\n\nhead value:", head_value, "head_pos:", list(head_pos), "dir:", direction)
-        masks = []
+        # masks = []
         for i, action in enumerate(bp.Policy.ACTIONS):
             temp_direction = bp.Policy.TURNS[direction][action]
             new_head = list(head_pos.move(temp_direction))
             # print("\nnew dir:", temp_direction, "new head_pos:", new_head)
 
             if new_head[1]==0:
-                temp_board = np.roll(board,1,axis=1)
-                new_head[1]+=1
+                temp_board = np.roll(board,SHIFT,axis=1)
+                new_head[1]+=SHIFT
                 if new_head[0]==0:
-                    temp_board = np.roll(temp_board,1,axis=0)
-                    new_head[0]+=1
+                    temp_board = np.roll(temp_board,SHIFT,axis=0)
+                    new_head[0]+=SHIFT
                 elif new_head[0]==rows-1:
-                    temp_board = np.roll(temp_board,-1,axis=0)
-                    new_head[0]-=1
+                    temp_board = np.roll(temp_board,-SHIFT,axis=0)
+                    new_head[0]-=SHIFT
             elif new_head[1]==cols-1:
-                temp_board = np.roll(board,-1,axis=1)
-                new_head[1]-=1
+                temp_board = np.roll(board,-SHIFT,axis=1)
+                new_head[1]-=SHIFT
                 if new_head[0]==0:
-                    temp_board = np.roll(temp_board,1,axis=0)
-                    new_head[0]+=1
+                    temp_board = np.roll(temp_board,SHIFT,axis=0)
+                    new_head[0]+=SHIFT
                 elif new_head[0]==rows-1:
-                    temp_board = np.roll(temp_board,-1,axis=0)
-                    new_head[0]-=1
+                    temp_board = np.roll(temp_board,-SHIFT,axis=0)
+                    new_head[0]-=SHIFT
 
             else:
                 if new_head[0]==0:
-                    temp_board = np.roll(board,1,axis=0)
-                    new_head[0]+=1
+                    temp_board = np.roll(board,SHIFT,axis=0)
+                    new_head[0]+=SHIFT
                 elif new_head[0]==rows-1:
-                    temp_board = np.roll(board,-1,axis=0)
-                    new_head[0]-=1
+                    temp_board = np.roll(board,-SHIFT,axis=0)
+                    new_head[0]-=SHIFT
                 else:
                     temp_board = board.copy()
             crop_rotate_board = np.rot90(temp_board[new_head[0]-1:new_head[0]+2,new_head[1]-1:new_head[1]+2],ROTATIONS[temp_direction])
-            # print("crop:\n", crop_rotate_board)
             curr_board = crop_rotate_board.copy()
             curr_board[1,1] = head_value
-            # print("action:", action, "| curr board:\n", curr_board)
-            masks.append(curr_board)
+            # masks.append(curr_board)
             feature_vector[i,:] = self.get_vector(curr_board)
 
-        return feature_vector, masks
+        # return feature_vector, masks
+        return feature_vector
 
     def get_last_feature_vector(self, new_state, action):
-        feature_vector = np.zeros(NUM_OF_FEATURES*WINDOW_SIZE)
         board, head = new_state
         head_pos, direction = head
         rows, cols = board.shape
@@ -159,35 +154,36 @@ class Custom(bp.Policy):
         temp_direction = bp.Policy.TURNS[direction][action]
         new_head = list(head_pos.move(temp_direction))
         if new_head[1]==0:
-            temp_board = np.roll(board,1,axis=1)
-            new_head[1]+=1
+            temp_board = np.roll(board,SHIFT,axis=1)
+            new_head[1]+=SHIFT
             if new_head[0]==0:
-                temp_board = np.roll(temp_board,1,axis=0)
-                new_head[0]+=1
+                temp_board = np.roll(temp_board,SHIFT,axis=0)
+                new_head[0]+=SHIFT
             elif new_head[0]==rows-1:
-                temp_board = np.roll(temp_board,-1,axis=0)
-                new_head[0]-=1
+                temp_board = np.roll(temp_board,-SHIFT,axis=0)
+                new_head[0]-=SHIFT
         elif new_head[1]==cols-1:
-            temp_board = np.roll(board,-1,axis=1)
-            new_head[1]-=1
+            temp_board = np.roll(board,-SHIFT,axis=1)
+            new_head[1]-=SHIFT
             if new_head[0]==0:
-                temp_board = np.roll(temp_board,1,axis=0)
-                new_head[0]+=1
+                temp_board = np.roll(temp_board,SHIFT,axis=0)
+                new_head[0]+=SHIFT
             elif new_head[0]==rows-1:
-                temp_board = np.roll(temp_board,-1,axis=0)
-                new_head[0]-=1
+                temp_board = np.roll(temp_board,-SHIFT,axis=0)
+                new_head[0]-=SHIFT
         else:
             if new_head[0]==0:
-                temp_board = np.roll(board,1,axis=0)
-                new_head[0]+=1
+                temp_board = np.roll(board,SHIFT,axis=0)
+                new_head[0]+=SHIFT
             elif new_head[0]==rows-1:
-                temp_board = np.roll(board,-1,axis=0)
-                new_head[0]-=1
+                temp_board = np.roll(board,-SHIFT,axis=0)
+                new_head[0]-=SHIFT
             else:
                 temp_board = board.copy()
         crop_rotate_board = np.rot90(temp_board[new_head[0]-1:new_head[0]+2,new_head[1]-1:new_head[1]+2],ROTATIONS[temp_direction])
         crop_rotate_board[1,1] = head_value
-        return self.get_vector(crop_rotate_board), crop_rotate_board
+        # return self.get_vector(crop_rotate_board), crop_rotate_board
+        return self.get_vector(crop_rotate_board)
 
     def act(self, round, prev_state, prev_action, reward, new_state, too_slow):
         # epsilon decreases as we progress in the game
@@ -195,7 +191,8 @@ class Custom(bp.Policy):
             self.epsilon*=self.epsilon_rate
 
         # get 3 feature vectors and their masks (window around the head)
-        feature_vector, masks = self.get_feature_vector(new_state)
+        # feature_vector, masks = self.get_feature_vector(new_state)
+        feature_vector= self.get_feature_vector(new_state)
 
         # case of bad or good performence change size of batch
         if too_slow:
@@ -205,8 +202,8 @@ class Custom(bp.Policy):
 
         # preprocessing
         if prev_state and not too_slow:
-            self.masks[self.batch_counter] = self.last_mask     # save the last window
-            self.next_masks[self.batch_counter] = masks         # save the next windows
+            # self.masks[self.batch_counter] = self.last_mask     # save the last window
+            # self.next_masks[self.batch_counter] = masks         # save the next windows
             self.batch_states[self.batch_counter] = self.last_feature_vector    # save the last feature vectors
             self.next_step_feature_vectors[self.batch_counter] = feature_vector # save the current feature vector
             self.rewards[self.batch_counter] = reward                           # save the last rewars
@@ -228,12 +225,13 @@ class Custom(bp.Policy):
 
         action = bp.Policy.ACTIONS[argmax]      # get the action according to the high prediction
         self.last_feature_vector = feature_vector[argmax]   # get the feature vector of this prediction
-        self.last_mask = masks[argmax]          # keep the mask of the high prediction
+        # self.last_mask = masks[argmax]          # keep the mask of the high prediction
 
         if np.random.rand() < self.epsilon:
             action = np.random.choice(bp.Policy.ACTIONS)
             # keep the feature vector for the random action
-            self.last_feature_vector, self.last_mask = self.get_last_feature_vector(new_state, action)
+            # self.last_feature_vector, self.last_mask = self.get_last_feature_vector(new_state, action)
+            self.last_feature_vector = self.get_last_feature_vector(new_state, action)
             return action
         else:
             return action
